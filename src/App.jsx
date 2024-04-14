a9c88b108df0c1a5b6275d5a48923efc38ff06bd
ValentynSkyba <valentynskyba@gmail.com>
2024-04-14 21:41:31 +0100

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { RiH1 } from "react-icons/ri";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/about" element={<h1>About</h1>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
